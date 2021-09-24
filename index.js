dcdd9e5567ee987f073703e2fe415f2d120058fe
jichen <keriteal985@outlook.com>
2021-09-24 16:20:31 +0800

    Update

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
 require('dotenv').config();
 
-var express = require('express');
-var fs = require('fs');
-var web3js = require('web3');
+const express = require('express');
+const fs = require('fs');
+const web3js = require('web3');
+const Tx = require('@ethereumjs/tx');
 
-var app = express();
-var web3 = new web3js(new web3js.providers.HttpProvider('https://rpc-mumbai.maticvigil.com'));
+const app = express();
+const web3 = new web3js(new web3js.providers.HttpProvider('https://rpc-mumbai.maticvigil.com'));
 
-var contractAddress = process.env.CONTRACT_ADDRESS
-var contractAbi = readFileSync('./erc20/build/contracts/Bitspawn.json');
-const contract = new web3.eth.Contract(JSON.parse(contractAbi), contractAddress)
+const contractAddress = process.env.CONTRACT_ADDRESS;
+const contractAbi = readFileSync('./erc20/build/contracts/Bitspawn.json');
+const contract = new web3.eth.Contract(JSON.parse(contractAbi), contractAddress);
 
-async function signTransaction(fromAddress, toAddress, nonceNum, privKey) {
-    var rawTransaction = {
-        from: fromAddress,
-        nonce: web3.util.toHex(nonceNum),
-        gasLimit: web3.util.toHex(6000000),
-        gasPrice: web3.util.toHex(10e9),
-        to: toAddress,
-        value: web3.util.toHex(0),
-        data: ,
-        chainId: 80001//4:Rinkeby, 3:Ropsten, 1:mainnet, 80001:polygon testnet
-    }
+async function signTransaction(contractMethod, fromAddress, toAddress, nonceNum) {
+  const rawTransaction = {
+    from: fromAddress,
+    nonce: web3.util.toHex(nonceNum),
+    gasLimit: web3.util.toHex(6000000),
+    gasPrice: web3.util.toHex(10e9),
+    to: toAddress,
+    value: web3.util.toHex(0),
+    data: contractMethod.encodeAbi(),
+    chainId: 80001, // 4:Rinkeby, 3:Ropsten, 1:mainnet, 80001:polygon testnet
+  };
+  const tx = new Tx(rawTransaction);
+  tx.sign(process.env.PRIV_KEY);
+  const serializedTx = tx.serialize();
+  const receipt = await web3.eth.sendSignedTransaction(`0x${serializedTx.toString('hex')}`);
 }
 
-app.get('/balanceOf', function (req, res) {
+app.get('/balanceOf', (req, res) => {
 
-})
+});
 
-app.get('/mint', function (req, res) {
+app.get('/mint', (req, res) => {
 
-})
+});
 
-app.get('/transfer', function (req, res) {
+app.get('/transfer', (req, res) => {
 
-})
+});
 
 app.listen(8080);
